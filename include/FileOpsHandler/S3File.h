59e8123106d344dbca05bd34ecfab86b2c64bc92
Mac16661 <macy16661@gmail.com>
2024-09-29 20:42:18 +0530

    Fresh New Setup

--- include/FileOpsHandler/S3File.h
+++
@@ -1,6 +0,0 @@
-#ifndef S3FILE_H
-#define S3FILE_H
-
-//TODO: To read/write files from AWS S3 bucket (to be implemented)
-
-#endif
--- src/FileOpsHandler/File.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../include/FileOpsHandler/File.h"
-
-
-virtual File::~File();
-virtual Types::FileOpsStatus File::readFile(std::string& path, int startByte, char* buffer);
-virtual Types::FileOpsStatus File::writeFile(std::string& path, char* buffer);
